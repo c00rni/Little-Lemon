00efd87d13fc09c4e36b4cc8ae0ca61a308f7aa1
corni <gmigan.a@gmail.com>
2024-02-16 09:34:26 +0100

    Add meta data tags and clear index.html from is default comments

--- little-lemon/src/DishCard.js
+++ little-lemon/src/DishCard.js
@@ -1,12 +1,12 @@
 function DishCard() {
     return (
-        <div className="card">
+        <article className="card">
             <img src="" alt="" />
             <h2>Title</h2>
             <span>12.99</span>
             <p>Text</p>
             <a href="#reservations">Order a delivery</a>
-        </div>
+        </article>
     );
 }
 
