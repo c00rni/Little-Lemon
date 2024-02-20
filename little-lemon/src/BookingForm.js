146650c3a483619f554bd80624e6d77f0febd497
corni <gmigan.a@gmail.com>
2024-02-20 04:07:00 +0100

    Add interactive error messages to the reservation table

--- little-lemon/src/BookingForm.js
+++ little-lemon/src/BookingForm.js
@@ -1,48 +1,17 @@
-// import Header from './Header';
-// import Footer from './Footer';
-// import { Formik, Field, Form, ErrorMessage} from 'formik';
-// import DatePicker from "react-datepicker";
-// import * as Yup from 'yup';
-// import "react-datepicker/dist/react-datepicker.css";
 import React, { useState } from "react";
 import BookingSlot from "./BookingSlot";
+import { useFormik} from 'formik';
 import { useRef } from 'react';
 import { submitAPI } from "./fakeApi";
 import { useNavigate } from "react-router-dom";
+import { object, string, number } from 'yup';
 
 function BookingForm({availabaleTimes}) {
     const minimumDate = new Date().toISOString().split('T')[0];
     const navigate = useNavigate();
     const [times, dispatch] = availabaleTimes
     const [active, setActive] = useState(null);
-    const [values, setValues] = useState({
-        resDate: null,
-        resTime: null,
-        guests: 0,
-        occasion: null
-    });
     const inputRef = useRef();
-
-    const handleChange = event => {
-
-        if (event.target.name === "resDate") {
-            dispatch(event.target.value)
-        }
-
-        setValues(prevState => {
-            return {
-              ...prevState,
-              [event.target.name]: event.target.value
-            }
-        });
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (values.resDate && values.resTime && values.guests && values.occasion) {
-           submitForm(values)
-        }
-    }
 
     const submitForm = (formData) => {
         if (submitAPI(formData)) {
@@ -55,23 +24,50 @@ function BookingForm({availabaleTimes}) {
         inputRef.current.type = 'date';
     }
 
+    let userSchema = object({
+        resDate: string().required("Required"),
+        resTime: string().required("Required"),
+        guests: number().min(1, "Select a number of dinner").required("Required"),
+        occassion: string().optional()
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            resDate: "",
+            resTime: "",
+            guests: 0,
+            occasion: ""
+        },
+        validationSchema: userSchema,
+        onSubmit: values => submitForm(values),
+    });
+
     return (
         <>
-            <form onSubmit={handleSubmit}>
-
+            <form onSubmit={formik.handleSubmit}>
                 <input
-                value={values.resDate ? values.resDate : "Date"}
+                value={formik.values.resDate ? formik.values.resDate : "Date"}
                 onFocus={toDateInput}
                 name="resDate" min={minimumDate}
                 required={true}
-                onChange={handleChange}
+                onChange={(event)=> {
+                    dispatch(event.target.value);
+                    formik.handleChange(event);
+                }}
+                onBlur={formik.handleBlur}
                 id="bookingDate"
                 ref={inputRef}
                 />
-                <div name="resTime" onChange={handleChange} id="bookingTime">
-                    {values.resDate ? [...times].map((time, index) => <BookingSlot index={index} activeIndex={active} onClick={setActive} children={time} handleChange={handleChange} key={index}/>) : null}
+                {formik.touched.resDate && formik.errors.resDate ? (
+                    <div className="error errorDate">{formik.errors.resDate}</div>
+                ) : null}
+                <div id="bookingTime">
+                    {formik.values.resDate ? [...times].map((time, index) => <BookingSlot index={index} activeIndex={active} onClick={setActive} children={time} handleChange={formik.handleChange} key={index}/>) : null}
                 </div>
-                <select name="guests" defaultValue="Number of Diners" onChange={handleChange} id="guests">
+                {formik.touched.resTime && formik.errors.resTime ? (
+                    <div className="error errorTime">{formik.errors.resTime}</div>
+                ) : null}
+                <select name="guests" defaultValue="Number of Diners" onChange={formik.handleChange} onBlur={formik.handleBlur} id="guests">
                     <option disabled hidden>Number of Diners</option>
                     <option key={1} values={1}>1</option>
                     <option key={2} values={2}>2</option>
@@ -83,16 +79,23 @@ function BookingForm({availabaleTimes}) {
                     <option key={8} values={8}>8</option>
                     <option key={9} values={9}>More</option>
                 </select>
-                <select name="occasion" defaultValue="Occasion" onChange={handleChange} id="bookingOccasion">
+                {formik.touched.guests && formik.errors.guests ? (
+                    <div className="error errorGuests">{formik.errors.guests}</div>
+                ) : null}
+                <select name="occasion" onChange={formik.handleChange} onBlur={formik.handleBlur} id="bookingOccasion">
                     <option disabled hidden>Occasion</option>
                     <option value="No special occasion">No special occasion</option>
                     <option value="Birthday">Birthday</option>
                     <option value="Anniversary">Anniversary</option>
                 </select>
-
+                {formik.touched.occassion && formik.errors.occassion ? (
+                    <div className="error errorOccation">{formik.errors.occassion}</div>
+                ) : null}
                 <input type="submit" value="Let's go" className='btn btn-primary' id="bookingSubmit"/>
             </form>
         </>
+
+
     );
   }
 
